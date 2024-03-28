3414c7c14da425936d54346e004327bc08b79d01
Jatin Gawad <jatin.gawad@gmail.com>
2024-03-27 20:58:58 -0700

    jatin/ec2-sg-added

---
+++ TerraformCode/V1-EC2.tf
@@ -0,0 +1,11 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "appserver" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
+  key_name      = "awsproj"
+}
+
